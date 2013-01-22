e0a17fff979e9d87600e0332ad1075044ff43984
Jan Vlnas <git@jan.vlnas.cz>
2013-01-22 02:04:05 +0100

    Enable assets hash

--- config.rb
+++ config.rb
@@ -69,8 +69,10 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 
+  activate :asset_hash
+
   # Enable cache buster
-  activate :cache_buster
+  # activate :cache_buster
 
   # Use relative URLs
   activate :relative_assets
