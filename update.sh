dfe87c4d83f483e2e38c2f1e644e3b19209e88d0
Jan Vlnas <git@jan.vlnas.cz>
2013-06-06 21:52:46 +0200

    Update script: fix broken export using sed

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-curl http://amber.festivalfantazie.cz/exporty/condroid.php > amber.xml
+curl http://amber.festivalfantazie.cz/exporty/condroid.php | \
+sed -e 's/2013-0 /2013-0/g' -e 's/\+01:00/\+02:00/g' -e 's/[ \t]*$//' > amber.xml
+
 ./parse.rb amber.xml > data/program.yml
